eaddb2fd1f1496b114e9db49e8b9f1567fd2ffce
Raphael Cerqueira <rcerqueira@vr.com.br>
2021-02-04 19:19:22 -0300

    Initial Commit

---
+++ Clubhouse/ClubhouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubhouseApp.swift
+//  Clubhouse
+//
+//  Created by Raphael Cerqueira on 04/02/21.
+//
+
+import SwiftUI
+
+@main
+struct ClubhouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clubhouse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clubhouse
+//
+//  Created by Raphael Cerqueira on 04/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
